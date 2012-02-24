59aa320da052b983bc4ce6efc03fd8d2ae72fa0d
Thijs Damen <thijs@egeniq.com>
2012-02-24 19:29:23 +0100

    Fi rst version

---
+++ jquery.clearinput-0.1.js
@@ -0,0 +1,106 @@
+/*
+ * jQuery ClearInput Plugin
+ * 
+ * Clears default values from input fields on submit.
+ * 
+ * Version: 0.1, 24 February, 2012
+ * Author: Thijs Damen <http://thijsdamen.nl, http://twitter.com/@/thijsdamen>
+ */
+(function($) {
+
+    $.clearInput = function(element, options) {
+
+        var defaults = {
+            clearTextFields: 			 true, 				 // if true clears text fields
+            clearPasswordFields:		 true, 		   	     // if true clears password fields
+            clearTextAreas:				 true, 			     // if true clears textareas
+            clearAll:					 true,				 // Ignores the class name setting and clears all fields regardless of class (still respects clearTextFields, clearPasswordFields and clearTextFields settings)
+            clearClassName:				 'clear-input',  	 // Default class name for elements to be cleared
+        	replaceValues:				 true,				 // Whether or not fields that have not been entered should be replaced
+            replaceValueDataName:	     'replace-with', 	 // Default data-* value to replace non-entered values with (only applies if replaceValues is true)
+            valueStorageDataName:        'default-value',    // Default data-* name script will store values to.
+            clearOnSubmit:				 true				 // Wheter or not default form values should be emptied before submit (still replaces values if replaceValues is true)
+        }
+
+        // current instance of the object
+        var plugin = this;
+        plugin.settings = {}
+
+        // References to the element plugin is attached to
+        var $element = $(element), 
+             element = element;    
+        
+        plugin.init = function() {
+            plugin.settings = $.extend({}, defaults, options);
+            
+            // Plugin only works on forms
+            if (!$element.is('form')) {
+            	return;
+            }
+
+          	$('input, textarea', $element).each(function(index, formElement) {
+           		$formElement = $(formElement);
+           		clearElement($formElement);
+           	});
+          	
+          	$element.submit(function() {
+              	$('input, textarea', $element).each(function(index, formElement) {
+               		$formElement = $(formElement);
+               		replaceElementValue($formElement);
+               	});
+          		return true;
+          	});
+        }
+        
+        var clearElement = function($formElement) { 
+        	console.log($formElement.data());
+     	    if (plugin.settings.clearAll || $formElement.hasClass(plugin.settings.clearClassName)) {
+    		    if (($formElement.attr('type') == 'text' && plugin.settings.clearTextFields)
+    			 || ($formElement.attr('type') == 'password' && plugin.settings.clearPasswordFields)
+    			 || ($formElement.is('textarea') && plugin.settings.clearTextAreas)) {
+				    $formElement.data(plugin.settings.valueStorageDataName, $formElement.val());
+				   
+				    $formElement.focus(function() {
+					    $focusedElement = $(this);
+					    if ($focusedElement.val() == $focusedElement.data(plugin.settings.valueStorageDataName)) {
+						    $focusedElement.val('');
+					    }        					   
+				    });
+				   
+				    $formElement.blur(function() {
+					    $blurredElement = $(this);
+					    if ($blurredElement.val().length == 0) {
+						    $blurredElement.val($blurredElement.data(plugin.settings.valueStorageDataName));
+					    }
+				    });
+    		    }
+    	    }
+        }
+        
+        var replaceElementValue = function($formElement) {
+   		    if (($formElement.attr('type') == 'text' && plugin.settings.clearTextFields)
+   	    			 || ($formElement.attr('type') == 'password' && plugin.settings.clearPasswordFields)
+   	    			 || ($formElement.is('textarea') && plugin.settings.clearTextAreas)) {
+    		    	
+   		    	if ($formElement.val() == $formElement.data(plugin.settings.valueStorageDataName)) {
+   		    		if (plugin.settings.replaceValues && $formElement.data(plugin.settings.replaceValueDataName)) {
+						$formElement.val($formElement.data(plugin.settings.replaceValueDataName));
+					} else if (plugin.settings.clearOnSubmit) {
+						$formElement.val('');
+					}
+   		    	}
+   		    }
+        }
+        
+        plugin.init();
+    }
+
+    $.fn.clearInput = function(options) {
+        return this.each(function() {
+            if (undefined == $(this).data('clearInput')) {
+                var plugin = new $.clearInput(this, options);
+                $(this).data('clearInput', plugin);
+            }
+        });
+    }
+})(jQuery);
---
+++ readme.md
@@ -0,0 +1,24 @@
+[jQuery clear input](http://github.com/thijsdamen/jquery-clearinput/)
+==================================================
+
+About this plugin
+--------------------------------------
+
+This plugin was created to clearn input fields when focused. It is custom these days to use a placeholder value in an input field. And for every single you you needed 3 or 4 lines of code.
+No more, mostly to suit my own needs and hopefully someone else's this plugin was created.
+
+Goals of the plugin
+--------------------------------------
+Make it <b>very</b> easy to use.
+
+
+Functionality
+--------------------------------------
+- Empty a form field when focused (if placeholder value is still entered)
+- Repopulate form field if blurred (if no value was entered)
+- Submit empty values (if placeholder value is still entered)
+- Submit custom values (if placeholder value is still entered)
+
+Requirements
+--------------------------------------
+[jQuery](http://jquery.com/) 1.4.4+
